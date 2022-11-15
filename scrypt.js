555a11a1b50f67a506d440171be6cb5c809a64f8
unknown <arnav22112003@gmail.com>
2022-11-15 21:32:39 +0530

    initial commit

---
+++ scrypt.js
@@ -0,0 +1,22 @@
+// var i = 0;
+// var txt = "Arnav Kaushik";
+// var toggle=true;    
+// var j=txt.length;
+// var speed = 300;
+
+// function write() {
+//     if (i <= txt.length) {
+//         document.getElementById("name").innerHTML = txt.substring(0,i);
+//         i++;
+//         setTimeout(write, speed);}
+//         else{
+//             setTimeout(write, speed);
+//             document.getElementById("name").innerHTML = txt.substring(0,j);
+//             if(j==0){
+//                 i=0;
+//                 j=txt.length;
+//             }
+//             j--;
+//         }
+//     }   
+// write();
